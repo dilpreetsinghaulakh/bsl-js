c86ea07ed551e66b4dcc8e64777d1001220c840d
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2023-10-29 22:26:11 +0530

    Added find in Tree class

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ class Tree {
 
         return node;
     }
+
+    find(data) {
+        let current = this.root;
+        while (current) {
+            if (data === current.data) {
+                return current;
+            }
+            if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return null;
+    }
 }
 
 let tree = new Tree();
